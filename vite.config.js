ab8e8450c340c9b8dcdc187bc0e3f3bee1878359
João Victor Guimarães <jvguimaraes2709@gmail.com>
2025-11-13 21:01:50 -0300

    sla

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  // 🚨 MUDANÇA NECESSÁRIA: O BASE deve ser o nome do seu repositório
+  base: '/api-project-activity-sla/', 
 })
